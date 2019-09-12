67583d3da97665f977dd477cb20f6c1fca9fd6e6
Jean Ferreira <jferreira91@Jeans-Air.connect>
2019-09-12 19:09:22 +0100

    Blog functionality and Polish

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    # __str__ shows the Title of each Blog Post on /admin/blog
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:240]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
